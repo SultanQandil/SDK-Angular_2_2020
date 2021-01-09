92852b6b720eb151f7f38e3a59eb1860b83e01de
malakalsalman <malakalsalman@users.noreply.github.com>
2021-01-09 19:33:02 +0200

    Modules & Views

--- firstProject/karma.conf.js
+++ firstProject/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/firstProject'),
+      dir: require('path').join(__dirname, './coverage/FirstProject'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -1,18 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule } from '@angular/core'
+import {AppComponent} from './app.component'
+import {BrowserModule} from'@angular/platform-browser'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations : [AppComponent] , // set of component reltae to this module(array)
+    imports: [BrowserModule],
+    bootstrap :[AppComponent], // set of components of are bootsraped when this module is boostrabed 
 })
-export class AppModule { }
+export class AppModule {}
