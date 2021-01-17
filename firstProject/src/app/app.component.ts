c67987d50f8839ff9150c19a12ddcca19438c868
malakalsalman <malakalsalman@users.noreply.github.com>
2021-01-17 21:26:34 +0200

    Template Syntax

--- firstProject/src/app/app.component.ts
+++ firstProject/src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { from } from 'rxjs';
 import {Component} from '@angular/core'
+import { FormsModule } from '@angular/forms';
 
-
+interface employee{
+    name :string ;
+    salary : number ;
+}
 @Component({
-    selector:"app",
+    selector:"app-root",
     templateUrl:"./app.component.html"
 
 })
-export class AppComponent{}
+export class AppComponent{
+    isVslid :boolean = true; 
+    username :string  ="";
+    msg :string ="anngular ";
+    num : number = 5;
+    imgUrl = "https://via.placeholder.com/150"; // prop Biding
+    hide : boolean = true;
+    say(){ // event Biding
+        alert("hii") ;
+    }
+    sayHi(name :string){ // event Biding
+        alert("hii"+name) ;
+    }
+
+   
+   emp : Array<employee> ;
+   constructor (){
+       this.emp = [
+           {name: "sultan " , salary : 600} ,
+           {name: "fares " , salary : 61100}
+       ]
+   }
+
+reload(){
+    this.emp = [
+        {name: "sultan " , salary : 600} ,
+        {name: "fares " , salary : 61100},
+      {name: "malak " , salary : 61100},
+      
+
+    ]
+}
+
+track (index:number ,emp:employee){
+    return emp.name
+}
+
+color =["red","green","black"];
+n:string="";
+// reload1(){
+//     this.emp = [
+//         {name: "sultan " , salary : 600} ,
+//         {name: "fares " , salary : 61100},
+//       {name: "malak " , salary : 61100},
+//       {name: "malak " , salary : 61100},
+
+      
+
+//     ]
+// }
+
+
+}
--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import {NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms';
 import {AppComponent} from './app.component'
 import {BrowserModule} from'@angular/platform-browser'
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations : [AppComponent] , // set of component reltae to this module(array)
-    imports: [BrowserModule],
+    imports: [BrowserModule , CommonModule , FormsModule],
     bootstrap :[AppComponent], // set of components of are bootsraped when this module is boostrabed 
+   
 })
 export class AppModule {}
