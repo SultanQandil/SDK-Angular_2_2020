bb1077c373cc604ddd9681a713b8b0090bd88cad
malakalsalman <malakalsalman@users.noreply.github.com>
2021-01-20 18:57:25 +0200

    Component interaction

--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -5,9 +5,11 @@ import {BrowserModule} from'@angular/platform-browser'
 import { CommonModule } from '@angular/common';
 import { IntroComponent } from './intro/intro.component';
 import { ListmploeesComponent } from './employees/listmploees/listmploees.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
-    declarations : [AppComponent, IntroComponent, ListmploeesComponent] , // set of component reltae to this module(array)
+    declarations : [AppComponent, IntroComponent, ListmploeesComponent, ParentComponent, ChildComponent] , // set of component reltae to this module(array)
     imports: [BrowserModule , CommonModule , FormsModule],
     bootstrap :[AppComponent], // set of components of are bootsraped when this module is boostrabed 
    
